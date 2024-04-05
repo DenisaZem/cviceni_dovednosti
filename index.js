33ddf32d101e6d08a2b2767ea0c13d295462652b
DenisaZem <xdenisa.zemanova@gmail.com>
2024-04-05 09:43:47 +0200

    Vypracováno

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('funguju!');
+
+const answer1 =  Number(prompt("Ohodnoť své dovednosti v HTML číslem od 1 o 100"))
+const answer2 =  Number(prompt("Ohodnoť své dovednosti v CSS číslem od 1 o 100"))
+const answer3 =  Number(prompt("Ohodnoť své dovednosti v Javascriptu číslem od 1 o 100"))
+
+const updateSkill =(ID, value)=>{
+    const getID = document.getElementById(ID).querySelector(".skill__value")
+    getID.textContent = (`${value}/100`)
+
+    const css_skill_progress = document.getElementById(ID).querySelector(".skill__progress ")
+    css_skill_progress.style.width = `${value}%`
+}
+
+updateSkill("skill1",answer1)
+updateSkill("skill2",answer2)
+updateSkill("skill3",answer3)
+ 
+
+
+
+
+
